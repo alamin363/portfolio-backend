a64429163459b46455e175ad397bb2df6a5f698d
alaminpk360 <alaminpkalaminpk5@gmail.com>
2022-12-10 12:59:36 +0600

    portfol=bacend start

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => console.log(`${port} is running`))
